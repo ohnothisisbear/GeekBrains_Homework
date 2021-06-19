6ba47a45a315e0bc6a97918001d70fe7fd607133
ohnothisisbear <31559393+ohnothisisbear@users.noreply.github.com>
2021-06-19 20:58:25 +0300

    Add files via upload

---
+++ seconds_minutes.py
@@ -0,0 +1,12 @@
+duration = 60
+seconds = (duration % 60, "seconds")
+minutes = round(duration / 60)
+##hours = #
+#days = duration % 86400
+#weeks = duration % 604800
+#months = 2629743
+#years = 31556926
+print(seconds)
+print(minutes)
+#print(hours)
+
