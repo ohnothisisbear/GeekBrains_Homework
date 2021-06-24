767ed6e7499764e6bfc74696febcb8c6ef693bea
unknown <ohnothisisbear@gmail.com>
2021-06-24 12:33:22 +0300

    homework_version
    with no second task

--- procent.py
+++ procent.py
@@ -1,11 +1,28 @@
-number = 20
-number = 3
-number = 1
-if number > 4:
-    print(str(number) + " процентов")
-elif number > 20:
-    print("Введите число от 1 до 20. Введеное вами число больше 20")
+#цикл для вывода всех склонений.
+
+
+number = int(input("Введите число от 1 до 20"))
+
+if number > 20:
+    print("Число больше 20")
+elif number < 1:
+    print("Число меньше 1")
+elif number > 4:
+    print(number, "процентов")
 elif number < 2:
-    print(str(number) + " процент")
+    print(number, "процент")
 else:
-    print(str(number) + " процента")
+    print(number, "процента")
+
+
+print("Все склонения:")
+number = 0
+
+while number < 20 or number == 20:
+    if number > 4:
+        print(number, "процентов")
+    elif number < 2:
+        print(number, "процент")
+    else:
+        print(number, "процента")
+    number += 1
